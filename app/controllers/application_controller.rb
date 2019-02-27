d840818cc30e51e9720d0558b7723fc0f3ef9cfe
Stephen Wright <scw248@gmail.com>
2019-02-27 15:58:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
       @team_name = @team.name
       @team_motto = @team.motto
      
-      params[:team][:members].each do |details|
+      params[:team][:member].each do |details|
         Member.new(details)
       end
      
